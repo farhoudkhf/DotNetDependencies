4760031ede28ef68ac0b418c249521f022c07849
Farhoud Khoshnoud <farhoudkhf@gmail.com>
2025-01-13 23:43:01 -0800

    c01

--- obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64453feb6266a60a5446384d6019218dd5469a9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
